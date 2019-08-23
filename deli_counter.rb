51262802b6bfe25c48d153b4e68ba4c590314be3
Baktiar Miah <baktiar.m@gmail.com>
2019-08-23 20:08:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,9 @@
-def line 
-  new_arr=[]
+def line (line)
   if line.empty?
     puts "The line is currently empty."
   
 else 
-  
+  new_arr=[]
   line.each_with_index  {|name, index|  new_arr.push( "#{index+1}. #{name}") }
     return "The line is currently: " + new_arr.join(' ')
   end
